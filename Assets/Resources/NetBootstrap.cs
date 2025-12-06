bb3bf8955a9c72d17358feb96c2406478cbd7210
nraesalmi <niklas.raesalmi@gmail.com>
2025-12-06 16:49:44 +0200

    now character spawns in a correct spot and works

--- Assets/Resources/NetBootstrap.cs
+++ Assets/Resources/NetBootstrap.cs
@@ -37,10 +37,10 @@ public class NetBootstrap : MonoBehaviourPunCallbacks
 
         int i = PhotonNetwork.CurrentRoom.PlayerCount - 1;
         Vector3[] spawns = {
-            new Vector3(-5.201f, 5.348f, -30.15f),
-            new Vector3(-5.201f, 5.348f, -30.15f),
-            new Vector3(-8.473f, 5.348f, -30.15f),
-            new Vector3(-10.232f, 5.348f, -30.15f),
+            new Vector3(-5.201f, 5f, -29.85f),
+            new Vector3(-5.201f, 5f, -29.85f),
+            new Vector3(-8.473f, 5f, -29.85f),
+            new Vector3(-10.232f, 5f, -29.85f),
         };
         Vector3 pos = spawns[Mathf.Clamp(i, 0, spawns.Length - 1)];
 
